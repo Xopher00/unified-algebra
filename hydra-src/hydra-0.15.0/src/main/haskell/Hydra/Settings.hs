fd86bea8167ce69e42169fd856164a358d1aef2c
Xopher00 <pricekr1221@gmail.com>
2026-06-01 12:20:49 +0300

    Remove hydra-src from tracking

--- hydra-src/hydra-0.15.0/src/main/haskell/Hydra/Settings.hs
+++
@@ -1,5 +0,0 @@
--- | The one non-generated Hydra kernel module. It contains constants which are set individually in each Hydra implementation.
-
-module Hydra.Settings where
-
-hydraDebug = True :: Bool
